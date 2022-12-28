bc859f9c07ba3e28abd0f24a8fb155997dc5a907
adrianavinicki <adriana.vinicki@gmail.com>
2022-12-28 20:52:32 -0300

    28.12

--- api/src/controllers/getApiData.js
+++ api/src/controllers/getApiData.js
@@ -23,29 +23,63 @@ IMPORTANTE: Dentro de la Ruta Principal se deben mostrar tanto los videjuegos tr
 
 const getApiData = async () => {
   // debe ser asyncronico porque no sabes cuanto tarda la api en contestar
-  const apiInfo = await axios.get(`${API_URL}?key=${API_KEY}&page_size=1000`,{headers:{'Accept-Encoding':'identity'}})// ?al colocar el page size toma un limite  de 40 ids no importa la cantidad que coloco
-  /*.then((res) => console.log(res));*/
-    .then((res) =>
-    res.data.results.map((video) => {
-      return {
-        id: video.id,
-        name: video.name,
-        slug: video.slug,
-        released: video.released,
-        rating: video.rating,
-        platforms: video.platforms.map(el => el.platform.name),
-        background_image: video.background_image,
-        genre: video.genres.map(el => el.name)
-      };
-    })
+  let apiInfo = [];
+
+  const pageOne = await axios.get(`${API_URL}?key=${API_KEY}&page=1`, {
+    headers: { "Accept-Encoding": "identity" },
+  }); // ?al colocar el page size toma un limite  de 40 ids no importa la cantidad que coloco
+  const pageTwo = await axios.get(`${API_URL}?key=${API_KEY}&page=2`, {
+    headers: { "Accept-Encoding": "identity" },
+  });
+  const pageThree = await axios.get(`${API_URL}?key=${API_KEY}&page=3`, {
+    headers: { "Accept-Encoding": "identity" },
+  });
+  const pageFour = await axios.get(`${API_URL}?key=${API_KEY}&page=4`, {
+    headers: { "Accept-Encoding": "identity" },
+  });
+  const pageFive = await axios.get(`${API_URL}?key=${API_KEY}&page=5`, {
+    headers: { "Accept-Encoding": "identity" },
+  });
+
+  apiInfo = pageOne.data.results.concat(
+    pageTwo.data.results,
+    pageThree.data.results,
+    pageFour.data.results,
+    pageFive.data.results
   );
-  return apiInfo;
+
+  let apiInfoTotal = apiInfo.map((video) => {
+    return {
+      id: video.id,
+      name: video.name,
+      slug: video.slug,
+      released: video.released,
+      rating: video.rating,
+      platforms: video.platforms.map((el) => el.platform.name),
+      background_image: video.background_image,
+      genre: video.genres.map((el) => el.name),
+    };
+  });
+
+  return apiInfoTotal;
 };
-
-
-
-
 
 module.exports = { getApiData };
 
-
+/*const apiInfo = await axios.get(`${API_URL}?key=${API_KEY}&page_size=1000`,{headers:{'Accept-Encoding':'identity'}})// ?al colocar el page size toma un limite  de 40 ids no importa la cantidad que coloco
+//.then((res) => console.log(res));
+  .then((res) =>
+  res.data.results.map((video) => {
+    return {
+      id: video.id,
+      name: video.name,
+      slug: video.slug,
+      released: video.released,
+      rating: video.rating,
+      platforms: video.platforms.map(el => el.platform.name),
+      background_image: video.background_image,
+      genre: video.genres.map(el => el.name)
+    };
+  })
+);
+return apiInfo;*/
